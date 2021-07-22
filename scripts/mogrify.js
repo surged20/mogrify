dee956c3fc620773a35ae455e4e070cb271db49d
Surge <surged20@gmail.com>
2021-07-22 08:27:46 -0400

    cap feature and actor data

--- scripts/mogrify.js
+++ scripts/mogrify.js
@@ -50,6 +50,11 @@ function doItemRemove(actor, item) {
   return false;
 }
 
+// Capitalize first letter in p1 and replace in match
+function capP1(match, p1, p2, p3, offset, string) {
+  return match.replace(p1, p1[0].toUpperCase() + p1.substring(1));
+}
+
 function doEntityRegexes(entity, desc) {
   // Apply per-type regexes to text
   if (CONFIG.JANITOR.mogrifyData[entity.type]) {
@@ -57,7 +62,10 @@ function doEntityRegexes(entity, desc) {
     if (typeData?.regexes) {
       typeData.regexes.forEach(e => {
         if (e?.global)
-          desc = desc.replaceAll(RegExp(e.regex, 'g'), e.string);
+          if (e?.cap)
+            desc = desc.replaceAll(RegExp(e.regex, 'g'), capP1);
+          else
+            desc = desc.replaceAll(RegExp(e.regex, 'g'), e.string);
         else
           desc = desc.replace(RegExp(e.regex), e.string);
       });
@@ -86,6 +94,20 @@ function removeEntityFlags(entity) {
     const typeData = CONFIG.JANITOR.mogrifyData["all"];
     if (typeData?.removeFlags)
       typeData.removeFlags.forEach(scope => entity.update({ [`flags.-=${scope}`] : null }));
+  }
+}
+
+async function updateActorData(actor) {
+  if (!CONFIG.JANITOR.mogrifyData[actor.type])
+    return;
+  const type = CONFIG.JANITOR.mogrifyData[actor.type];
+
+  if (type?.data) {
+    type.data.forEach(e => {
+      if (e.actorNames.includes(actor.name)) {
+        e.data.forEach(p => actor.update({[`${p.prop}`]: p.value}));
+      }
+    });
   }
 }
 
@@ -143,6 +165,7 @@ function processItem(item) {
 function processActor(actor) {
 
   removeEntityFlags(actor);
+  updateActorData(actor);
   doItemsAdd(actor);
 
   actor.items.forEach(item => {
